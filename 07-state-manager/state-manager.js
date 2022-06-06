df8f5f36db95488654b7238394fb528a3abc66cb
Magesh Kuppan <tkmagesh77@gmail.com>
2022-06-06 17:22:49 +0530

    using more than one reducer (with combineReducers)

--- 07-state-manager/state-manager.js
+++ 07-state-manager/state-manager.js
@@ -46,5 +46,30 @@ const StateManager = (function(){
         return actionDispatchers;
     }
 
-    return { createStore, bindActionCreators };
+   /*  function combineReducers(reducers){
+        const reducerKeys = Object.keys(reducers)
+        return function(state = {}, action) {
+            const nextState = {};
+            for (let i = 0; i < reducerKeys.length; i++){
+                const key = reducerKeys[i]
+                const reducer = reducers[key]
+                const prevStateForKey = state[key]
+                const nextStateForKey = reducer(prevStateForKey, action)
+                nextState[key] = nextStateForKey
+            }
+            return nextState;
+        }
+    } */
+
+    function combineReducers(reducers){
+        return function(state = {}, action) {
+            const nextState = {};
+            for (let key in reducers){
+                nextState[key] = reducers[key](state[key], action)
+            }
+            return nextState;
+        }
+    }
+
+    return { createStore, bindActionCreators, combineReducers };
 })()
