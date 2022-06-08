be0e8f3651df4cd7609f8ac83a17a0464d93c822
Magesh Kuppan <tkmagesh77@gmail.com>
2022-06-08 15:19:56 +0530

    associating the bugs with projects

--- bug-tracker-app/src/bugs/components/BugItem.jsx
+++ bug-tracker-app/src/bugs/components/BugItem.jsx
@@ -4,7 +4,10 @@ const BugItem = ({bug, toggle, remove}) => {
             <span 
                 className={ "bugname " + (bug.isClosed ? "closed" : "")}
                 onClick={() => toggle(bug)}
-            >{bug.name}</span>
+            >
+                {bug.name}
+            </span>
+            <small> [ project name ] </small>
             <div className="datetime">{bug.createdAt}</div>
             <button onClick={() => remove(bug)}>Remove</button>
         </li>
