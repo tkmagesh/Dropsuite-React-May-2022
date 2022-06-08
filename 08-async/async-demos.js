7ca01dc4ad9c894aa50086e82f70f4de5baea8ee
Magesh Kuppan <tkmagesh77@gmail.com>
2022-06-08 17:46:43 +0530

    async programming in JS (callback, promise & async-await)

---
+++ 08-async/async-demos.js
@@ -0,0 +1,77 @@
+
+(function(){
+
+    function addSync(x,y){
+        console.log(`   [@service] processing ${x} and ${y}`)
+        let result = x + y
+        console.log(`   [@service] returning result`)
+        return result
+    }
+
+    function addSyncClient(x,y){
+        console.log(`[@client] invoking the service`)
+        let result = addSync(x,y)
+        console.log(`[@client] result = ${result}`)
+    }
+
+    window['addSyncClient'] = addSyncClient;
+
+    function addAsync(x,y, callback){
+        console.log(`   [@service] processing ${x} and ${y}`)
+        setTimeout(function(){
+            let result = x + y
+            console.log(`   [@service] returning result`)
+            callback(result)
+        }, 5000);
+    }
+
+    function addAsyncClient(x,y){
+        console.log(`[@client] invoking the service`)
+        addAsync(x,y, function(result){
+            console.log(`[@client] result = ${result}`)
+        })
+    }
+
+    window['addAsyncClient'] = addAsyncClient;
+
+    function addAsyncPromise(x,y){
+        console.log(`   [@service] processing ${x} and ${y}`)
+        let promise = new Promise(function(resolveFn, rejectFn){
+            setTimeout(function(){
+                let result = x + y
+                console.log(`   [@service] returning result`)
+                resolveFn(result)
+            }, 5000);
+        });
+        return promise;
+    }
+
+    //window['addAsyncPromise'] = addAsyncPromise;
+
+    
+
+    function addAsyncPromiseClient(x,y){
+        console.log(`[@client] invoking the service`);
+        var p = addAsyncPromise(x,y)
+        var p2 = p.then(function(result){
+            console.log(`[@client] result = ${result}`); 
+            return result * 2;  
+        });
+        return p2;
+        
+    }
+
+    window['addAsyncPromiseClient'] = addAsyncPromiseClient
+
+    async function addAsyncPromiseClient2(x,y){
+        console.log(`[@client] invoking the service`)
+        let result = await addAsyncPromise(x,y)
+        console.log(`[@client] result = ${result}`)
+        return result * 2
+    }
+
+    window['addAsyncPromiseClient2'] = addAsyncPromiseClient2
+    
+
+})();
+
--- bug-tracker-app/src/store/index.js
+++ bug-tracker-app/src/store/index.js
@@ -2,6 +2,7 @@ import { combineReducers, createStore, applyMiddleware} from 'redux'
 /* import spinnerReducer from '../spinner/reducers/spinnerReducer'; */
 import bugsReducer from '../bugs/reducers/bugsReducer';
 import projectsReducer from '../projects/reducers/projectsReducer';
+import logger from 'redux-logger';
 
 const rootReducer = combineReducers({
     bugsState : bugsReducer,
@@ -19,16 +20,17 @@ function loggerMiddleware(store){
 } 
 */
 
+/* 
 const loggerMiddleware = store => next => action => {
     console.group(action.type);
     console.log('Before', store.getState())
     console.log('Action', action)
     next(action);
     console.log('After', store.getState())
-    console.groupEnd();
-    
-}
+    console.groupEnd(); 
+} 
+*/
 
-const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 export default store;
